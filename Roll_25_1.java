224bd28f77e2767d2f9d510c5f476b09895f4249
Akshay3311 <akshaykharpas111@gmail.com>
2024-12-05 05:06:42 +0530

    Add files via upload

---
+++ Roll_25_1.java
@@ -0,0 +1,8 @@
+
+public class Roll_25_1 {
+
+    public static void main(String[] args) {
+        System.out.println("");
+        System.out.println("Hello Friends!! Welcome to My Java World!!");
+    } // By Akshay Kharpas
+}
